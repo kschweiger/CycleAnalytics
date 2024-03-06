9fcae0f6dbc4597729f9d211b93c9357cc600842
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-03-06 19:54:54 +0100

    refactor: Moved location overview html template

--- cycle_analytics/locations.py
+++ cycle_analytics/locations.py
@@ -33,7 +33,7 @@ def overview() -> str | Response:
     location_markers = convert_locations_to_markers(get_locations(), True)
 
     return render_template(
-        "locations.html",
+        "locations/overview.html",
         active_page="locations",
         location_markers=location_markers,
     )
